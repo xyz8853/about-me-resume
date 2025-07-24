74f732f269c3abf760e7fd1f80927e5c1196b925
xyz8853 <ktejas8853@gmail.com>
2025-07-24 16:44:05 +0530

    again Update event.js

--- event.js
+++ event.js
@@ -27,3 +27,94 @@ let sId = setInterval( ()=>{
      }) 
     iNo++;
 },1000);
+
+/**page popup */
+    
+
+let admissionPopup = document.querySelector('#admissionPopup');
+
+setTimeout( ()=>{
+    admissionPopup.removeAttribute('class');
+    admissionPopup.setAttribute('class','admission-popup');
+},1000);
+
+let closePopup = document.querySelector('#closePopup');
+    closePopup.addEventListener('click', function(event){
+        admissionPopup.setAttribute('class','admission');
+    });
+
+    let submitBtn = document.querySelector(".submit-btn-popup");
+    let callBtn = document.querySelector(".call-btn");
+    let whatsappBtn = document.querySelector('.whatsapp-btn');
+
+        submitBtn.addEventListener('mouseover', function(event){
+            submitBtn.textContent='Thank You';
+        });
+         submitBtn.addEventListener('mouseout', function(event){
+            submitBtn.textContent='Subscribe';
+        });
+
+        callBtn.addEventListener('mouseover', function(){
+            // callBtn.textContent=' Call Me';
+            callBtn.textContent=' 8853292074';
+            let i = document.createElement('i');
+            i.setAttribute('class','fa fa-phone');
+            callBtn.prepend(i);
+        })
+        callBtn.addEventListener('mouseout', function(){
+           callBtn.textContent=' Call Now';
+            let i = document.createElement('i');
+            i.setAttribute('class','fa fa-phone');
+            callBtn.prepend(i);
+        })
+
+        whatsappBtn.addEventListener('mouseover', function(){
+            whatsappBtn.textContent=' 8853292074';
+            let i = document.createElement('i');
+            i.setAttribute('class','fa fa-whatsapp');
+            whatsappBtn.prepend(i);
+            
+        });
+        
+        whatsappBtn.addEventListener('mouseout', function(){
+            whatsappBtn.textContent=' WhatsApp';
+            let i = document.createElement('i');
+            i.setAttribute('class','fa fa-whatsapp');
+            whatsappBtn.prepend(i);
+            
+        });
+/**end page popup */
+
+/**our-work {java-script} */
+let allWork = document.querySelectorAll('.cources-item');   
+    allWork.forEach( (element,index)=>{
+         let id;
+         let value1;
+         let value2;
+         let str;
+         let index2;
+         element.addEventListener('mouseover', function(event){
+             value1 = element.children;
+             value2 = value1[1].children;
+             str = value2[0].innerHTML;
+             value2[0].innerHTML='';
+             let addText='';
+             index2=0;
+             id = setInterval( ()=>{
+                if(str.length-1>=index2){
+                    addText = addText+str[index2];
+                    value2[0].textContent=addText;
+                    if(str.length-1 == index2){
+                        clearInterval(id);
+                        index2=0;
+                    }
+                    index2++;
+                }
+             },80);
+             
+         })
+          element.addEventListener('mouseout', function(event){
+                value2[0].textContent=str;
+                clearInterval(id);
+          });
+    })
